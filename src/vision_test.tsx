776566898ca9f240810e83ea6d87a32ff0c12c3e
inxole <inxole@gmail.com>
2023-05-09 01:27:12 +0900

    create base script

--- src/vision_test.tsx
+++ src/vision_test.tsx
@@ -1,5 +1,8 @@
 import { Button, Box } from "@mui/material"
 import Grid from '@mui/material/Unstable_Grid2'
+import Landolt_C from './Model/Landolt_C'
+import { Canvas } from "@react-three/fiber"
+import { Suspense } from "react"
 
 const Vision = () => {
 
@@ -14,6 +17,14 @@ const Vision = () => {
                     </Grid>
                     <Grid xs={6}>
                         <Button>{'⇦'}</Button>
+                        <Canvas>
+                            <Suspense>
+                                <group >
+                                    <pointLight intensity={1} position={[0, 0, 100]} />
+                                </group >
+                                <Landolt_C rotation={[Math.PI / 2, 0, 0]} scale={3} />
+                            </Suspense>
+                        </Canvas>
                         <Button>{'⇨'}</Button>
                     </Grid>
                     <Grid xs={12}>
