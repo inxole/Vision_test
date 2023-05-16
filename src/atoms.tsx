df4446df0f8f1994a24b40f4b51ff2d4d9e00768
inxole <inxole@gmail.com>
2023-05-16 23:37:23 +0900

    add atoms and double click button bug

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -2,3 +2,5 @@ import { atom } from "recoil";
 
 export const ScaleSize = atom({ key: 'SS', default: 4 })
 export const RandomRotation = atom({ key: 'RR', default: 0 })
+export const CurrentAngle = atom({ key: 'CA', default: 0 })
+export const ButtonDirection = atom({ key: 'BD', default: '' })
--- src/vision_test.tsx
+++ src/vision_test.tsx
@@ -3,33 +3,46 @@ import Button from '@mui/material/Button'
 import Box from '@mui/material/Box'
 import Landolt_C from './Model/Landolt_C'
 import { Canvas } from '@react-three/fiber'
-import { Suspense } from 'react'
+import { Suspense, useEffect } from 'react'
 import { PerspectiveCamera } from '@react-three/drei'
 import { useRecoilState } from 'recoil'
-import { RandomRotation, ScaleSize } from './atoms'
+import { ButtonDirection, RandomRotation, ScaleSize } from './atoms'
 
 const Vision = () => {
     const [scale, setScale] = useRecoilState(ScaleSize)
+    const [rotationY, setRotationY] = useRecoilState(RandomRotation)
+    const [name, setName] = useRecoilState(ButtonDirection)
     const scaleValues = [4, 3, 2, 0.7, 0.5, 0.4, 0.3, 0.2, 0.1]
     const currentIndex = scaleValues.indexOf(scale)
-    const [, setRotationY] = useRecoilState(RandomRotation)
 
-    const Click_Button = () => {
-        const nextIndex = (currentIndex + 1) % scaleValues.length;
+    const Click_Button = async (n: string) => {
+        const nextIndex = (currentIndex + 1) % scaleValues.length
+        setName(n)
+        setName((x) => { return x })
         if (scale == 0.1) {
             setScale(4)
             setRotationY(getRandomRotationY())
         } else {
-            setScale(scaleValues[nextIndex])
-            setRotationY(getRandomRotationY())
+            if (rotationY == 0 && name == 'right' ||
+                rotationY == Math.PI / 2 && name == 'up' ||
+                rotationY == Math.PI && name == 'left' ||
+                rotationY == (3 * Math.PI) / 2 && name == 'down'
+            ) {
+                setScale(scaleValues[nextIndex])
+                setRotationY(getRandomRotationY())
+            }
         }
     }
 
     const getRandomRotationY = () => {
-        const randomIndex = Math.floor(Math.random() * 4); // 0から3の乱数を生成
-        const rotationYValues = [0, Math.PI / 2, Math.PI, (3 * Math.PI) / 2]; // 0°, 90°, 180°, 270°
-        return rotationYValues[randomIndex];
+        const randomIndex = Math.floor(Math.random() * 4)// 0から3の乱数を生成
+        const rotationYValues = [0, Math.PI / 2, Math.PI, (3 * Math.PI) / 2]// 0°, 90°, 180°, 270°
+        return rotationYValues[randomIndex]
     }
+
+    useEffect(() => {
+        setRotationY(getRandomRotationY())
+    }, [])
 
     return (
         <div style={{ display: 'flex', justifyContent: 'center' }}>
@@ -41,18 +54,18 @@ const Vision = () => {
                             <canvas aria-hidden width={50} height={60}></canvas>
                         </Grid>
                         <Grid xs={4}>
-                            <Button variant="contained" size="large" style={{ width: '50px', height: '60px' }} onClick={Click_Button}>{'⇧'}</Button>
+                            <Button variant="contained" size="large" style={{ width: '50px', height: '60px' }} onClick={() => { Click_Button('up') }}>{'⇧'}</Button>
                         </Grid>
                     </Grid>
                     <Grid container spacing={2} >
                         <Grid xs={4}>
-                            <Button variant="contained" size="large" style={{ width: '50px', height: '60px' }} onClick={Click_Button}>{'⇦'}</Button>
+                            <Button variant="contained" size="large" style={{ width: '50px', height: '60px' }} onClick={() => { Click_Button('left') }}>{'⇦'}</Button>
                         </Grid>
                         <Grid xs={4}>
                             <canvas aria-hidden width={50} height={60}></canvas>
                         </Grid>
                         <Grid xs={4}>
-                            <Button variant="contained" size="large" style={{ width: '50px', height: '60px' }} onClick={Click_Button}>{'⇨'}</Button>
+                            <Button variant="contained" size="large" style={{ width: '50px', height: '60px' }} onClick={() => { Click_Button('right') }}>{'⇨'}</Button>
                         </Grid>
                     </Grid>
                     <Grid container spacing={2} >
@@ -60,7 +73,7 @@ const Vision = () => {
                             <canvas aria-hidden width={50} height={60}></canvas>
                         </Grid>
                         <Grid xs={4}>
-                            <Button variant="contained" size="large" style={{ width: '50px', height: '60px' }} onClick={Click_Button}>{'⇩'}</Button>
+                            <Button variant="contained" size="large" style={{ width: '50px', height: '60px' }} onClick={() => { Click_Button('down') }}>{'⇩'}</Button>
                         </Grid>
                     </Grid>
                 </Grid>
@@ -72,6 +85,7 @@ const Vision = () => {
 export const Landolt_C_view = () => {
     const [scale,] = useRecoilState(ScaleSize)
     const [rotationY,] = useRecoilState(RandomRotation)
+
     return (
         <div style={{ display: 'flex', justifyContent: 'center' }}>
             <Box component="span" pt={20}>
